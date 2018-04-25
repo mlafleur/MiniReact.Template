858b4ec6c6347c6f2c6b699026b81278d6be2ba6
Marc LaFleur <marcla@microsoft.com>
2018-04-25 16:46:27 -0400

    Improved configuration

--- content/webpack.config.js
+++ content/webpack.config.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 module.exports = {
   context: path.resolve(__dirname, "Client"),
+  devtool: "source-map",
+  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx", ".json"] },
   entry: {
     main: "./main.tsx"
   },
@@ -14,12 +16,26 @@ module.exports = {
     libraryTarget: "var",
     library: "Client"
   },
-  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+
   module: {
     rules: [
-      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+      // Used to load CSS styles
       { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
-  devtool: "source-map"
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM"
+  }
 };
