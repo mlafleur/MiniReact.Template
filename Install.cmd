262b1961ed7cef5818efc441c52c03e9689ddc53
Marc LaFleur <marcla@microsoft.com>
2019-09-16 11:16:52 -0400

    Fixed version

--- Install.cmd
+++ Install.cmd
@@ -1,5 +1,5 @@
 dotnet build content
 nuget.exe pack
 dotnet new --uninstall MiniReact.Template
-dotnet new --install .\MiniReact.Template.0.1.0.nupkg
+dotnet new --install .\MiniReact.Template.0.2.0.nupkg
 dotnet clean content
